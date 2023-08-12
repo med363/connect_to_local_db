78d44d7206690b532d8ce406e835eb8e714427e1
med363 <blibechmedamine@gmail.com>
2023-08-12 12:57:31 +0100

    crud..

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -61,7 +61,7 @@ exports.updateUser=(req, res) => {
     }
 //delete one medication
 exports.deleteUser=(req, res) => {
-    User.findOneAndUpdate(
+    User.findOneAndDelete(
         {_id: req.params.id}
     ).then(data => res.send(data)).catch(err => {
             res.status(500).send({ message: err.message || "something wrong when DELETE medicals" })
